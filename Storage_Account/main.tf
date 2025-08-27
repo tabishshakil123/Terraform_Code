6af546c168badd8c1ac693075f66c4640ebe6e1a
Tabish shakil <tshakil123@gmail.com>
2025-08-28 00:23:23 +0530

    commit

--- Storage_Account/main.tf
+++ Storage_Account/main.tf
@@ -1,10 +1,10 @@
 resource "azurerm_resource_group" "RG-block" {
-  name     = "RG2"
+  name     = "RG1"
   location = "east us"
 }
 # Storage Account
 resource "azurerm_storage_account" "storage-block" {
-  name                     = "tabishstorageaccount01"   # Azure par globally unique hona chahiye
+  name                     = "tabishstorage012345"   # Azure par globally unique hona chahiye
   resource_group_name      = azurerm_resource_group.RG-block.name
   location                 = azurerm_resource_group.RG-block.location
   account_tier             = "Standard"
