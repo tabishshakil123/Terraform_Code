4f8dde142629181eaa36167041c09482a80d5954
Tabish shakil <tshakil123@gmail.com>
2025-08-27 23:53:47 +0530

    commit

--- Storage_Account/main.tf
+++ Storage_Account/main.tf
@@ -1,17 +1,20 @@
-resource "azurerm_resource_group" "RG-block1" {
+resource "azurerm_resource_group" "RG-block" {
   name     = "RG2"
   location = "east us"
 }
 # Storage Account
-resource "azurerm_storage_account" "example" {
+resource "azurerm_storage_account" "storage-block" {
   name                     = "tabishstorageaccount01"   # Azure par globally unique hona chahiye
-  resource_group_name      = azurerm_resource_group.RG-block1.name
-  location                 = azurerm_resource_group.RG-block1.location
+  resource_group_name      = azurerm_resource_group.RG-block.name
+  location                 = azurerm_resource_group.RG-block.location
   account_tier             = "Standard"
   account_replication_type = "GRS"
 
-  tags = {
-    environment = "staging"
-  }
+  
 }
 
+resource "azurerm_storage_container" "container-block" {
+  name                  = "terraformcontainer"
+  storage_account_id    = azurerm_storage_account.storage-block.id
+  container_access_type = "private"
+}
